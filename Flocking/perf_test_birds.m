607d4254e71de262efc00b480c58a43340a05099
Siddhartha Banerjee <banerjs.sid@gmail.com>
2013-04-25 02:24:58 -0400

    Working on data collection for Flocking

---
+++ Flocking/perf_test_birds.m
@@ -0,0 +1,47 @@
+%%PERF_TEST_BIRDS.m
+% Due to the commonality in the code between the various simulations of
+% birds, we can have a common wrapper to do performance testing on the
+% birds.
+
+clear all; clc;
+
+% Set the variable to disable lower-level animations
+STAT_COLLECTING = 1;
+
+% Set the parameters for simulation
+NOISE = 0; % There should be no noise in the model <- It's USELESS
+FIELD = 50; % The field of the model
+CONFINE = 0; % Confine the model? 0->No, 1->Yes
+REPULSE = 0; % Repulsion type? 0->None, 1->Linear, 2->Non-linear
+
+% Tests to perform - vary population and radii
+NUM_TRIALS = 10; % Number of trials to run for each simulation
+poptests = [100, 200, 300, 400, 500]; % Tests to run on the population
+radtests = [1, 5, 10, 20, 30, 40]; % Tests to run for RADII
+buckets = zeros(size(poptests,2), size(radtests,2)); % Collect results here
+% DISCLAIMER: This may not be able to test confined birds, repulsed/
+% attracted birds...
+
+% Perform the tests
+disp('Beginning performance testing');
+for POPULATION = poptests
+    fprintf('Initializing population to %d\n', POPULATION);
+    pindex = find(POPULATION == poptests, 1, 'first');
+    
+    for RADIUS = radtests
+        fprintf('Initializing radius to %d\n', RADIUS);
+        rindex = find(RADIUS == radtests, 1, 'first');
+        
+        for m = 1:NUM_TRIALS
+            disp({'On trial ',num2str(m),' for radius ',num2str(RADIUS), ' and population ', num2str(POPULATION)});
+            
+            if CONFINE == 0 && REPULSE == 0 % Clean Vicsek model
+                simulate_vicsek_birds; % Perform the simulation
+            end
+
+            buckets(pindex, rindex) = buckets(pindex, rindex) + i;
+        end
+        buckets(pindex,rindex) = buckets(pindex,rindex)/NUM_TRIALS;
+    end
+end
+disp('End of performance test');
--- Flocking/simulate_vicsek_birds.m
+++ Flocking/simulate_vicsek_birds.m
@@ -2,7 +2,9 @@
 % This script is to simulate self propelled particles in a manner
 % described in the Vicsek paper
 
-clear all; clc;
+if ~exist('STAT_COLLECTING', 'var') || STAT_COLLECTING == 0
+    clear all; clc;
+end
 
 % Setup timing parameters for the model
 T = 100; % Total time of simulation
@@ -10,17 +12,21 @@ dT = 0.1; % Timestep size
 t = 0:dT:T; % Array of times that the simulation is to run for
 
 % Initialize Movie Parameters
-reruns = 1;
-fps = 1/dT;
-frames = moviein(size(t,2));
-SAVE_MOVIE = 0;
+if ~exist('STAT_COLLECTING', 'var') || STAT_COLLECTING == 0
+    reruns = 1;
+    fps = 1/dT;
+    frames = moviein(size(t,2));
+    SAVE_MOVIE = 0;
+end
 
 % Setup the model parameters
-POPULATION = 400; % Number of birds to simulate
-NOISE = 0; % Magnitude of Noise
+if ~exist('STAT_COLLECTING', 'var') || STAT_COLLECTING == 0
+    POPULATION = 400; % Number of birds to simulate
+    NOISE = 0; % Magnitude of Noise
+    FIELD = 50; % Size of the arena
+    RADIUS = 5; % Field of interaction
+end
 SPEED = 5; % Magnitude of the velocity
-FIELD = 50; % Size of the arena
-RADIUS = 5; % Field of interaction
 
 % Initialize the birds
 theta = rand(1,POPULATION)*2*pi;
@@ -30,44 +36,45 @@ positions = rand(2,POPULATION).*FIELD;
 modulo = @(x,n) (x - n*floor(x/n));
 
 % For loop to do the simulation
+otheta = theta + 1;
+
 for i = 1:size(t,2)
+    if exist('STAT_COLLECTING', 'var') && STAT_COLLECTING > 0
+        if otheta == theta
+            break;
+        end
+    end
+    
     % Plot the positions of the birds
-    plot(positions(1,:), positions(2,:), '.');
-    axis([0 FIELD 0 FIELD]);
-    title(['N = ', num2str(POPULATION), ', R = ', num2str(RADIUS)]);
-    frames(:,i) = getframe;
+    if ~exist('STAT_COLLECTING', 'var') || STAT_COLLECTING == 0
+        plot(positions(1,:), positions(2,:), '.');
+        axis([0 FIELD 0 FIELD]);
+        title(['N = ', num2str(POPULATION), ', R = ', num2str(RADIUS)]);
+        frames(:,i) = getframe;
+    end
+    
+    otheta = theta; % Store the previous heading of everyone
     
     % Update the heading and position of all the birds
-    %positions(1,:) = positions(1,:)+SPEED*dT.*cos(theta);
     positions(1,:) = modulo(positions(1,:)+SPEED*dT.*cos(theta), FIELD);
-    %positions(2,:) = positions(2,:)+SPEED*dT.*sin(theta);
     positions(2,:) = modulo(positions(2,:)+SPEED*dT.*sin(theta), FIELD);
 
     for j = 1:POPULATION
         neighbours = ((abs(positions(1,:)-positions(1,j)) < RADIUS) & (abs(positions(2,:)-positions(2,j)) < RADIUS));
-        %if positions(:,j) >= 0 & positions(:,j) <= FIELD
-        theta(j) = modulo(mean(theta(neighbours)) + NOISE*randn(), 2*pi);
-        %end
+        theta(j) = modulo(mean(otheta(neighbours)) + NOISE*randn(), 2*pi);
     end
     
-    % Apply confinement rules
-    %need1 = (positions(1,:)<=0 | positions(1,:)>=FIELD);
-    %need2 = (positions(2,:)<=0 | positions(2,:)>=FIELD);
-    %need = need1 & need2;
-    %%theta(need1) = pi - theta(need1);
-    %theta(need1 & ~need) = pi - theta(need1 & ~need);
-    %theta(need2 & ~need) = 3*pi/2 - theta(need2 & ~need);
-    %theta(need) = pi + theta(need);
 end
 % endfor
 
 % Plot the birds
-figure;
-plot(positions(1,:), positions(2,:), '.');
-axis([0 FIELD 0 FIELD]);
-title(['Final Position, N = ', num2str(POPULATION)]);
+if ~exist('STAT_COLLECTING', 'var') || STAT_COLLECTING == 0
+    plot(positions(1,:), positions(2,:), '.');
+    axis([0 FIELD 0 FIELD]);
+    title(['Final Position, N = ', num2str(POPULATION)]);
 
-% Save the movie if asked for
-if SAVE_MOVIE == 1
-    movie2avi(frames, strcat('Population',num2str(POPULATION),'Radius',num2str(RADIUS), '.avi'));
+    % Save the movie if asked for
+    if SAVE_MOVIE == 1
+        movie2avi(frames, strcat('Population',num2str(POPULATION),'Radius',num2str(RADIUS), '.avi'));
+    end
 end
